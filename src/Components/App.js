675035e59340f4a44cd45e1df2a88e7489dc808c
Alex Pilewski <pilewski.alex@gmail.com>
2017-02-16 12:51:47 -0700

    Add initial auth setup

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,16 @@ import Header from './Header';
 import CodeContainer from './CodeContainer';
 import TestContainer from './TestContainer';
 import '../App.css';
+import AuthService from '../utils/AuthService'
+
+const auth = new AuthService('nN29JzO2WI7XO7Ra5L5muICb1NPj5k59', 'pilewski.auth0.com');
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header auth={auth}/>
         <CodeContainer/>
         <TestContainer/>
       </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../Header.css';
+import AuthService from '../utils/AuthService'
 
 class Header extends Component {
   render() {
+    const { auth } = this.props
+
     return (
       <header className="Header">
         <h1>IronFest</h1>
-        
-        <button>Log In</button>
+
+        <button onClick={auth.login.bind(this)}>>Log In</button>
       </header>
     );
   }
