86733652895d95ccce21da6924f756089f73e20e
MilkMan90 <mlkaufman747@gmail.com>
2017-02-28 15:55:46 -0700

    some fixes

--- server/tmp/test.js
+++ server/tmp/test.js
@@ -1,22 +1,22 @@
 function example(){
 	return 'taylor rocks'
-} ; var { assert } = require('../chai');describe('Example Test', function() {
+}
+
+function nickRocks(){
+	return 10
+}
+
+nickRocks() ; var { assert } = require('../chai');describe('Example Test', function() {
   it('should be a function', function() {
-    assert.isFunction(example)
+    assert.isFunction(nickRocks)
   })
   it('should return taylor rocks', function() {
     assert.equal(example(), 'taylor rocks')
   })
-    it('should be a function', function() {
-    assert.isFunction(example)
-  })
-  it('should return taylor rocks', function() {
+    it('should return taylor rock!s', function() {
     assert.equal(example(), 'taylor rocks')
-  })
-    it('should be a function', function() {
-    assert.isFunction(example)
-  })
-  it('should return taylor rocks', function() {
-    assert.equal(example(), 'taylor rocks')
+    assert.isNull(null, 'there was no error');
+
   })
 })
+
