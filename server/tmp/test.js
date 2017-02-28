f1027da7465a7d1db15a7106225f7fe3e15c3bde
MilkMan90 <mlkaufman747@gmail.com>
2017-02-28 13:42:10 -0700

    tooltips, assertions added

--- server/tmp/test.js
+++ server/tmp/test.js
@@ -1,10 +1,14 @@
 function example(){
 	return 'taylor rocks'
-} ; var { assert } = require('../chai');describe('Example Test', function() {
+}
+
+example() ; var { assert } = require('../chai');describe('Example Test', function() {
   it('should be a function', function() {
     assert.isFunction(example)
   })
   it('should return taylor rocks', function() {
-    assert.equal(example(), 'taylord rocks')
+    assert.equal(example(), 'taylor dsarocks')
   })
+  
+  efeas
 })
---
+++ src/utils/assertSyntax.js
@@ -0,0 +1,59 @@
+const AssertCode = [
+  {
+    name: 'assert',
+    doc: `assert(expression, [message])`,
+    desc: 'Write your own test expression',
+    code: `assert('foo' !== 'bar', 'foo is not bar')`
+  },
+  {
+    name: 'isOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is truthy',
+    code: `assert.isOk('everything', 'everything is ok will pass');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+  {
+    name: 'isNotOk',
+    doc: `assert(object, [message])`,
+    desc: 'Asserts that object is falsey',
+    code: `assert.isNotOk('everything', 'this will fail');`
+  },
+
+]
+
+export default AssertCode;
